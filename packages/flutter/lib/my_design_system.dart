7f09180ce7f67617c363f0642b0f445e6625a635
bongjava <bongjava@gmail.com>
2026-08-15 10:37:49 +0900

    Add packages/flutter/lib/my_design_system.dart

---
+++ packages/flutter/lib/my_design_system.dart
@@ -0,0 +1,6 @@
+class MyDesignTokens {
+  static const colorBrandPrimary = 0xFF2563EB;
+  static const colorBrandSecondary = 0xFF14B8A6;
+  static const colorBrandAccent = 0xFFF97316;
+  static const radiusMd = 8.0;
+}
