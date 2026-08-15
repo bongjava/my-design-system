f973cbb1455694d290a09228410bad0129a54a5f
bongjava <bongjava@gmail.com>
2026-08-15 10:38:26 +0900

    Add packages/swiftui/Sources/MyDesignSystem/Tokens.swift

---
+++ packages/swiftui/Sources/MyDesignSystem/Tokens.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+public enum MyDesignTokens {
+    public static let colorBrandPrimary = Color(red: 37 / 255, green: 99 / 255, blue: 235 / 255)
+    public static let colorBrandSecondary = Color(red: 20 / 255, green: 184 / 255, blue: 166 / 255)
+    public static let colorBrandAccent = Color(red: 249 / 255, green: 115 / 255, blue: 22 / 255)
+    public static let radiusMd: CGFloat = 8
+}
